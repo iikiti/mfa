3eb4f9453e191493dd045b57c8cec9edee1fd51a
Jim Robinson <jimbo2150@gmail.com>
2024-01-19 09:23:04 -0500

    - Fixed incorrect method declarations

--- src/Authentication/Strategy/EmailTokenStrategy.php
+++ src/Authentication/Strategy/EmailTokenStrategy.php
@@ -2,21 +2,26 @@
 
 namespace iikiti\MfaBundle\Authentication\Strategy;
 
+use iikiti\MfaBundle\Authentication\Interface\ChallengeInterface;
+
 class EmailTokenStrategy extends AbstractTokenStrategy
 {
-	public function generateChallenge(): string
-	{
+	public function generateChallenge(
+		#[\SensitiveParameter] string $secret = null
+	): ChallengeInterface {
 	}
 
-	public function issueChallenge(): void
-	{
+	public function issueChallenge(
+		ChallengeInterface $challenge = null
+	): void {
 	}
 
-	public function validateChallenge(): bool
-	{
+	public function validateChallenge(
+		ChallengeInterface $challenge = null
+	): bool {
 	}
 
-	public function generateSecret(): null
+	public function generateSecret(): string
 	{
 	}
 }
