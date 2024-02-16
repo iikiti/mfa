8762f40296a5a67ff0040658e63fab52d33b86b3
Jim Robinson <jimbo2150@gmail.com>
2024-02-16 14:23:47 -0500

    - Removed unused code

--- src/iikitiMultifactorAuthenticationBundle.php
+++ src/iikitiMultifactorAuthenticationBundle.php
@@ -2,7 +2,6 @@
 
 namespace iikiti\MfaBundle;
 
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -13,17 +12,6 @@ class iikitiMultifactorAuthenticationBundle extends AbstractBundle
 	public function configure(DefinitionConfigurator $definition): void
 	{
 		parent::configure($definition);
-	}
-
-	private function __isDoctrineEntity(\ReflectionClass $reflectionClass): bool
-	{
-		if (
-			!empty($reflectionClass->getAttributes(Entity::class, \ReflectionAttribute::IS_INSTANCEOF))
-		) {
-			return true;
-		}
-
-		return false;
 	}
 
 	public function loadExtension(
