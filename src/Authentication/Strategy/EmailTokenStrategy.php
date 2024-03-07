610f48d1316bc047c6f3362ae2e5d10c59586d09
Jim Robinson <jimbo2150@gmail.com>
2024-03-07 18:52:15 -0500

    - Added email token challenge

--- src/Authentication/Strategy/EmailTokenStrategy.php
+++ src/Authentication/Strategy/EmailTokenStrategy.php
@@ -2,7 +2,10 @@
 
 namespace iikiti\MfaBundle\Authentication\Strategy;
 
+use iikiti\MfaBundle\Authentication\Challenge;
+use iikiti\MfaBundle\Authentication\Exception\AccessDeniedException;
 use iikiti\MfaBundle\Authentication\Interface\ChallengeInterface;
+use iikiti\MfaBundle\Authentication\TokenGenerator\StringTokenGenerator;
 
 /**
  * Email token strategy that handles the challenge via an email
@@ -10,11 +13,25 @@ use iikiti\MfaBundle\Authentication\Interface\ChallengeInterface;
  */
 class EmailTokenStrategy extends AbstractTokenStrategy
 {
+	protected StringTokenGenerator $stringGenerator;
+
+	public function __construct()
+	{
+		$this->stringGenerator = new StringTokenGenerator();
+	}
+
+	/**
+	 * Generate challenge (currently simple randomly generated 6-digit code).
+	 */
 	public function generateChallenge(
 		#[\SensitiveParameter] string $secret
 	): ChallengeInterface {
+		return new Challenge($this->generateSecret());
 	}
 
+	/**
+	 * Send email.
+	 */
 	public function issueChallenge(
 		ChallengeInterface $challenge
 	): void {
@@ -22,11 +39,18 @@ class EmailTokenStrategy extends AbstractTokenStrategy
 
 	public function validateChallenge(
 		ChallengeInterface $challenge,
-		#[\SensitiveParameter] string $secret
+		#[\SensitiveParameter] string $userInput
 	): array {
+		$errors = [];
+		if ($this->stringGenerator->validate($challenge->get(), $userInput)->count() > 0) {
+			$errors[] = new AccessDeniedException('Challenge validation failed.');
+		}
+
+		return $errors;
 	}
 
-	public function generateSecret(): string
+	public function generateSecret(int $length = 6): string
 	{
+		return $this->stringGenerator->generate(['min' => 0, 'max' => 9, 'length' => $length]);
 	}
 }
